1b27e4e68eb65ba8a9a721a98b2bf22e8e11d334
Vira <vira.romanko99@gmail.com>
2019-02-25 12:49:17 -0500

    Audio exampels

---
+++ js/main.js
@@ -0,0 +1,42 @@
+(() =>{
+	let aud = document.querySelector("audio"),
+	    play = document.querySelector("#playAudio"),
+	    pause = document.querySelector("#stopAudio"),
+	    rewind = document.querySelector("#rewindAudio"),
+	    tracks = document.querySelectorAll(".trackholder");
+
+
+	 function playAudio() {
+	 	aud.play();
+	 } 
+
+	 function stopAudio() {
+	 	aud.pause(true);
+	 	}
+
+	 	function rewindAudio() {
+	 		aud.currentTime = 0;
+	 		playAudio();
+	 	}
+
+
+	 	function switchAudioTrack(){
+	 		currentTrack = this.dataset.currenttrack;
+
+	 		aud.src = `audio/${currentTrack}`;
+	 		aud.load();
+	 		aud.play();
+	 	}
+
+
+
+	 	play.addEventListener("click", playAudio);
+	 	pause.addEventListener("click", stopAudio);
+	 	rewind.addEventListener("click", rewindAudio);
+
+
+	 	tracks.forEach(track => {
+	 		track.addEventListener("click", switchAudioTrack);
+	 	});
+
+})();
